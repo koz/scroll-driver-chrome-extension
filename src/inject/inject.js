f872bf54cb34dbd8349069f8b498616b93e23d12
Gabriel Kozlowaski <gkm.kozlowaski@gmail.com>
2019-09-19 14:33:57 -0300

    Implement initial version for the scrollbar

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -3,9 +3,29 @@ chrome.extension.sendMessage({}, function(response) {
     if (document.readyState === "complete") {
       clearInterval(readyStateCheckInterval);
 
-      const allElements = document.querySelectorAll("body > *");
-      let selectedElOriginalStyle = {};
-      let selectedEl;
+      // Inject styles
+      document
+        .getElementsByTagName("head")[0]
+        .append(
+          document
+            .createRange()
+            .createContextualFragment(
+              `<link href="${chrome.extension.getURL(
+                "src/inject/style.css"
+              )}" rel="stylesheet">`
+            )
+        );
+
+      // Inject layout
+      fetch(chrome.runtime.getURL("src/inject/layout.html"))
+        .then(data => data.text())
+        .then(response =>
+          document
+            .getElementsByTagName("html")[0]
+            .append(document.createRange().createContextualFragment(response))
+        );
+
+      // Add scroll listener
       document.addEventListener("scroll", event => {
         if (selectedEl) {
           selectedEl.style.setProperty(
@@ -15,31 +35,41 @@ chrome.extension.sendMessage({}, function(response) {
         }
       });
 
+      const allElements = document.querySelectorAll("body > *");
+      let selectedElOriginalStyle = {};
+      let selectedEl;
+
+      const YScrollSize = document.body.scrollHeight - window.innerHeight;
+
+      // Add click events to add animation to selected element;
       allElements.forEach(item =>
         item.addEventListener("click", e => {
+          e.preventDefault();
+          e.stopPropagation();
           if (selectedEl && Object.keys(selectedElOriginalStyle).length) {
             Object.keys(selectedElOriginalStyle).forEach(item => {
+              selectedEl.classList.remove("sd-animation");
               selectedEl.style.setProperty(item, selectedElOriginalStyle[item]);
             });
           }
 
           selectedEl = e.target;
           selectedElOriginalStyle = {
-            transition: selectedEl.style.transition,
             transform: selectedEl.style.transform
           };
-          selectedEl.style.setProperty("transition", "all 300ms ease");
+          selectedEl.classList.add("sd-animation");
         })
       );
 
+      // Add border to highlight which element is being selected
       allElements.forEach(item => {
         item.addEventListener("mouseover", e => {
           el = e.target;
-          el.style.setProperty("border", "1px dotted red");
+          el.classList.add("sd-hover-item");
         });
         item.addEventListener("mouseout", e => {
           el = e.target;
-          el.style.setProperty("border", "");
+          el.classList.remove("sd-hover-item");
         });
       });
     }
